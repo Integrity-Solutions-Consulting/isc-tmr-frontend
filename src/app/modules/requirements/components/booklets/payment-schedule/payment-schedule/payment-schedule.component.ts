28628d33291960874300144f0ddc2f59f4c01b5f
Xavier Tirado <xavier.tirado@integritysolutions.com.ec>
2026-01-08 10:43:32 -0500

    Booklets

---
+++ src/app/modules/requirements/components/booklets/payment-schedule/payment-schedule/payment-schedule.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-payment-schedule',
+  standalone: true,
+  imports: [],
+  templateUrl: './payment-schedule.component.html',
+  styleUrl: './payment-schedule.component.scss'
+})
+export class PaymentScheduleComponent {
+
+}
---
+++ src/app/modules/requirements/components/booklets/service-mode/service-mode/service-mode.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-service-mode',
+  standalone: true,
+  imports: [],
+  templateUrl: './service-mode.component.html',
+  styleUrl: './service-mode.component.scss'
+})
+export class ServiceModeComponent {
+
+}
