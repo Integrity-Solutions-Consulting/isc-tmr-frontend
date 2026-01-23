c9087106f3a442943b7a423a2653d3478189800e
Ángel Vivanco <angel.vivanco@integritysolutions.com.ec>
2026-01-23 11:04:06 -0500

    Cambio de version en el paquete localize y mejora del html de servicios y modalidad

--- src/app/modules/requirements/components/templates/service-mode/service-mode/service-mode.component.ts
+++ src/app/modules/requirements/components/templates/service-mode/service-mode/service-mode.component.ts
@@ -1,10 +1,16 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
-import { FormGroup } from '@angular/forms';
+import { FormGroup, ReactiveFormsModule } from '@angular/forms';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'service-mode',
   standalone: true,
-  imports: [],
+  imports: [
+    CommonModule,
+    ReactiveFormsModule,
+    MatIconModule
+  ],
   templateUrl: './service-mode.component.html',
   styleUrl: './service-mode.component.scss'
 })
